38bc50300412ae0855f03ef2fcc59d99f2d3e680
Lucas de Lima Bica <149081622+lucaslimabica@users.noreply.github.com>
2024-07-11 14:34:14 +0100

    Atelier Criado

--- reginaldoV35.py
+++ reginaldoV35.py
@@ -50,7 +50,7 @@ class App(tk.Tk):
         self.frames = {}
 
         # Inicialização dos frames
-        for F in (HomePage, EtapasFunisPage, CamposPage, AtividadesPage, UsersPage, TemplatesPage, APIsPage):
+        for F in (HomePage, EtapasFunisPage, CamposPage, AtividadesPage, UsersPage, TemplatesPage, APIsPage, TemplateAtelier):
             page_name = F.__name__
             frame = F(parent=container, controller=self, app_state=self.app_state)
             self.frames[page_name] = frame
@@ -77,17 +77,18 @@ class HomePage(ttk.Frame):
         self.grid_columnconfigure(0, weight=1)
         self.grid_columnconfigure(1, weight=1)
 
-        label = ttk.Label(self, text="Robot Engine Generator", font=('@Microsft YaHei UI', 20))
+        label = ttk.Label(self, text="Robot Engine Generator", font=('@Microsft YaHei UI', 20, "bold"))
         label.grid(row=0, column=0, columnspan=2, pady=10)
+        ttk.Label(self, text="(beta version)", font=('@Microsft YaHei UI', 8, "italic")).grid(row=1, column=0, columnspan=2, pady=0)
 
         label2 = ttk.Label(self, text="Insira o Token de API:")
-        label2.grid(row=1, column=0, columnspan=2, pady=10, padx=10)
+        label2.grid(row=2, column=0, columnspan=2, pady=10, padx=10)
 
-        self.api_token_entry = ttk.Entry(self)
-        self.api_token_entry.grid(row=2, column=0, columnspan=2, pady=10, padx=10)
+        self.api_token_entry = ttk.Entry(self, width=50)
+        self.api_token_entry.grid(row=3, column=0, columnspan=2, pady=10, padx=10)
 
         save_button = ttk.Button(self, text="Salvar Token", command=self.save_token, style="Custom.TButton")
-        save_button.grid(row=3, column=0, columnspan=2, pady=10)
+        save_button.grid(row=4, column=0, columnspan=2, pady=10)
 
         # Criação e posicionamento dos botões na grid
         button1 = ttk.Button(self, text="Etapas e Funis", 
@@ -102,17 +103,20 @@ class HomePage(ttk.Frame):
                              command=lambda: controller.show_frame("TemplatesPage"), style="Custom.TButton")
         button6 = ttk.Button(self, text="Galeria de APIs", 
                              command=lambda: controller.show_frame("APIsPage"), style="Custom.TButton")
+        button7 = ttk.Button(self, text="Atelier de Templates", 
+                             command=lambda: controller.show_frame("TemplateAtelier"), style="Custom.TButton")
 
         # Posicionando os botões na grid
-        ttk.Label(self, text="Criação do Ambiente Pipedrive").grid(row=4, column=0, padx=5, pady=10, columnspan=2)
-        button1.grid(row=5, column=0, padx=5, pady=10)
-        button2.grid(row=5, column=1, padx=5, pady=10)
-        ttk.Label(self, text="Personalização do Pipedrive").grid(row=6, column=0, padx=5, pady=10, columnspan=2)
-        button3.grid(row=7, column=0, padx=5, pady=10)
-        button4.grid(row=7, column=1, padx=5, pady=10)
-        ttk.Label(self, text="Ferramentas Avançadas").grid(row=8, column=0, padx=5, pady=10, columnspan=2)
-        button5.grid(row=9, column=0, padx=5, pady=10)
-        button6.grid(row=9, column=1, padx=5, pady=10)
+        ttk.Label(self, text="Criação do Ambiente Pipedrive").grid(row=5, column=0, padx=5, pady=10, columnspan=2)
+        button1.grid(row=6, column=0, padx=5, pady=10)
+        button2.grid(row=6, column=1, padx=5, pady=10)
+        ttk.Label(self, text="Personalização do Pipedrive").grid(row=7, column=0, padx=5, pady=10, columnspan=2)
+        button3.grid(row=8, column=0, padx=5, pady=10)
+        button4.grid(row=8, column=1, padx=5, pady=10)
+        ttk.Label(self, text="Ferramentas Avançadas").grid(row=9, column=0, padx=5, pady=10, columnspan=2)
+        button5.grid(row=10, column=0, padx=5, pady=10)
+        button6.grid(row=10, column=1, padx=5, pady=10)
+        button7.grid(row=11, column=0, padx=5, pady=10)
 
     def save_token(self):
         token = self.api_token_entry.get()
@@ -124,20 +128,20 @@ class EtapasFunisPage(ttk.Frame):
         self.controller = controller
         self.app_state = app_state
 
-        ttk.Label(self, text="Criação de Funil e Fases", font=('@Microsft YaHei UI', 20)).pack(pady=10)
+        ttk.Label(self, text="Criação de Funil e Fases", font=('@Microsft YaHei UI', 20, "bold")).pack(pady=10)
 
-        ttk.Label(self, text="Nome do Funil:").pack(padx=10, pady=5)
+        ttk.Label(self, text="Nome do Funil:", font=('@Microsft YaHei UI', 12)).pack(padx=10, pady=5)
         self.entrada_nome_funil = ttk.Entry(self, width=50)
         self.entrada_nome_funil.pack(padx=10, pady=5)
 
         self.botao_enviar_funil = ttk.Button(self, text="Criar Funil", command=self.criarFunil, style="Custom.TButton")
         self.botao_enviar_funil.pack(padx=10, pady=10)
 
-        ttk.Label(self, text="Etapas:").pack(padx=10, pady=5)
+        ttk.Label(self, text="Etapas:", font=('@Microsft YaHei UI', 12)).pack(padx=10, pady=5)
         self.entrada_nome_etapa = ttk.Entry(self, width=50)
         self.entrada_nome_etapa.pack(padx=10, pady=5)
 
-        self.selecao_funil = ttk.Label(self, text="Selecione um Funil")
+        self.selecao_funil = ttk.Label(self, text="Selecione um Funil", font=('@Microsft YaHei UI', 12))
         self.selecao_funil.pack(pady=10, padx=10)
 
         # Adicionando um Combobox (dropdown)
@@ -204,16 +208,16 @@ class CamposPage(ttk.Frame):
 
         ttk.Label(self, text="Criação de Campos", font=('@Microsft YaHei UI', 20)).pack(pady=10)
 
-        ttk.Label(self, text="Criar Campo:").pack(padx=10, pady=5)
+        ttk.Label(self, text="Criar Campo:", font=('@Microsft YaHei UI', 12)).pack(padx=10, pady=5)
         self.entrada_nome_campo = ttk.Entry(self, width=70)
         self.entrada_nome_campo.pack(padx=10, pady=5)
 
-        ttk.Label(self, text="Criar Opções (para Escolhas):").pack(padx=10, pady=5)
+        ttk.Label(self, text="Criar Opções (para Escolhas):", font=('@Microsft YaHei UI', 12)).pack(padx=10, pady=5)
         self.entrada_nome_ops = ttk.Entry(self, width=70)
         self.entrada_nome_ops.pack(padx=10, pady=5)
 
         # Adicionando um Combobox (dropdown)
-        ttk.Label(self, text="Selecione o Tipo:").pack(padx=10, pady=5)
+        ttk.Label(self, text="Selecione o Tipo:", font=('@Microsft YaHei UI', 12)).pack(padx=10, pady=5)
         self.variavel_dropdown_tipo = tk.StringVar()
         self.dropdown_tipo = ttk.Combobox(self, textvariable=self.variavel_dropdown_tipo, values=["Texto", "Escolha", "Data", "Moeda", "Status", "Numero", "Multipla Escolha", "Hora"], style='TCombobox')
         self.dropdown_tipo.pack(pady=5, padx=10)
@@ -268,9 +272,9 @@ class AtividadesPage(ttk.Frame):
         self.controller = controller
         self.app_state = app_state
 
-        ttk.Label(self, text="Criação de Atividades", font=('@Microsft YaHei UI', 20)).pack(pady=10)
+        ttk.Label(self, text="Criação de Atividades", font=('@Microsft YaHei UI', 20, "bold")).pack(pady=10)
 
-        ttk.Label(self, text="Nome da Atividade:").pack(padx=10, pady=5)
+        ttk.Label(self, text="Nome da Atividade:", font=('@Microsft YaHei UI', 12)).pack(padx=10, pady=5)
         self.entrada_nome_atv = ttk.Entry(self, width=70)
         self.entrada_nome_atv.pack(padx=10, pady=5)
 
@@ -309,13 +313,13 @@ class UsersPage(ttk.Frame):
         self.controller = controller
         self.app_state = app_state
 
-        ttk.Label(self, text="Criação de Usuários", font=('@Microsft YaHei UI', 20)).pack(pady=10)
+        ttk.Label(self, text="Criação de Usuários", font=('@Microsft YaHei UI', 22, "bold")).pack(pady=10)
 
-        ttk.Label(self, text="Nome do Usuário:").pack(padx=10, pady=5)
+        ttk.Label(self, text="Nome do Usuário:", font=('@Microsft YaHei UI', 12)).pack(padx=10, pady=5)
         self.entrada_nome_user = ttk.Entry(self, width=70)
         self.entrada_nome_user.pack(padx=10, pady=5)
 
-        ttk.Label(self, text="Email do Usuário:").pack(padx=10, pady=5)
+        ttk.Label(self, text="Email do Usuário:", font=('@Microsft YaHei UI', 12)).pack(padx=10, pady=5)
         self.entrada_email_user = ttk.Entry(self, width=70)
         self.entrada_email_user.pack(padx=10, pady=5)
 
@@ -348,9 +352,9 @@ class TemplatesPage(ttk.Frame):
         self.controller = controller
         self.app_state = app_state
 
-        ttk.Label(self, text="Galeria de Templates", font=('@Microsft YaHei UI', 20)).pack(pady=10)
+        ttk.Label(self, text="Galeria de Templates", font=('@Microsft YaHei UI', 20, "bold")).pack(pady=10)
 
-        ttk.Label(self, text="Template:").pack(padx=10, pady=5)
+        ttk.Label(self, text="Selecione o Template:", font=('@Microsft YaHei UI', 12)).pack(padx=10, pady=5)
 
         self.variavel_dropdown = tk.StringVar()
         self.dropdown = ttk.Combobox(self, textvariable=self.variavel_dropdown, values=REGistador001.listaTemplates(), style='TCombobox')
@@ -386,13 +390,77 @@ class TemplatesPage(ttk.Frame):
                             )
         REGistador001.fazer_LOG(acao=f"Uso de Template: {nome_modelo}", api=self.app_state.api_token)
 
+class TemplateAtelier(ttk.Frame):
+    def __init__(self, parent, controller, app_state):
+        super().__init__(parent)
+        self.controller = controller
+        self.app_state = app_state
+
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_rowconfigure(1, weight=1)
+        self.grid_rowconfigure(2, weight=1)
+        self.grid_rowconfigure(3, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=1)
+
+        label = ttk.Label(self, text="Atelier de Templates", font=('@Microsft YaHei UI', 20, "bold"))
+        label.grid(row=0, column=0, columnspan=2, pady=10)
+
+        ttk.Label(self, text="Nome do Template:", font=("@Microsft YaHei UI", 12)).grid(row=1, column=0, columnspan=2, pady=10)
+        self.entrada_nome_template = ttk.Entry(self, width=50).grid(row=2, column=0, columnspan=2, pady=10)
+
+        ttk.Label(self, text="Nome do Funil:", font=("@Microsft YaHei UI", 12)).grid(row=3, column=0, pady=10)
+        self.entrada_funil_template = ttk.Entry(self, width=50).grid(row=3, column=1, pady=10)
+
+        ttk.Label(self, text="Fases:", font=("@Microsft YaHei UI", 12)).grid(row=4, column=0, pady=10)
+        self.entrada_fases_template = ttk.Entry(self, width=50).grid(row=4, column=1, pady=10)         
+
+        ttk.Label(self, text="Campos Texto:", font=("@Microsft YaHei UI", 12)).grid(row=5, column=0, pady=10)
+        self.entrada_textos_template = ttk.Entry(self, width=50).grid(row=5, column=1, pady=10)
+
+        ttk.Label(self, text="Campos Número:", font=("@Microsft YaHei UI", 12)).grid(row=6, column=0, pady=10)
+        self.entrada_numeros_template = ttk.Entry(self, width=50).grid(row=6, column=1, pady=10)
+
+        ttk.Label(self, text="Campos Data:", font=("@Microsft YaHei UI", 12)).grid(row=7, column=0, pady=10)
+        self.entrada_datas_template = ttk.Entry(self, width=50).grid(row=7, column=1, pady=10)
+
+        ttk.Label(self, text="Campos Opção:", font=("@Microsft YaHei UI", 12)).grid(row=8, column=0, pady=10)
+        self.entrada_escolha_template = ttk.Entry(self, width=50).grid(row=8, column=1, pady=10)
+
+        ttk.Label(self, text="Opções (op1, op2, op3):", font=("@Microsft YaHei UI", 12)).grid(row=8, column=0, pady=10)
+        self.entrada_ops_template = ttk.Entry(self, width=50).grid(row=8, column=1, pady=10)
+
+        ttk.Label(self, text="Atividades (nome, icon):", font=("@Microsft YaHei UI", 12)).grid(row=9, column=0, pady=10)
+        self.entrada_atividades_template = ttk.Entry(self, width=50).grid(row=9, column=1, pady=10)
+
+        self.token_label = ttk.Label(self, text="")
+        #self.token_label.grid(row=10, columnspan=2, column=0, pady=10)
+
+        self.botao_enviar_template = ttk.Button(self, text="Salvar Template", command=self.salvarTemplate, style="Custom.TButton")
+        self.botao_enviar_template.grid(row=10, column=0, pady=10, columnspan=2)
+
+        self.button = ttk.Button(self, text="Voltar para a Página Inicial", 
+                           command=lambda: controller.show_frame("HomePage"), style="Custom.TButton")
+        self.button.grid(row=11, columnspan=2, column=0, pady=10)
+
+    def salvarTemplate(self):
+        pass
+
+    def tkraise(self, aboveThis=None):
+        if self.app_state.api_token:
+            self.token_label.config(text=f"Token de API: {self.app_state.api_token}")
+        else:
+            self.token_label.config(text="Token de API não definido")
+        super().tkraise(aboveThis)
+
+
 class APIsPage(ttk.Frame):
     def __init__(self, parent, controller, app_state):
         super().__init__(parent)
         self.controller = controller
         self.app_state = app_state
 
-        ttk.Label(self, text="Galeria de APIs", font=('@Microsft YaHei UI', 20)).pack(pady=10)
+        ttk.Label(self, text="Galeria de APIs", font=('@Microsft YaHei UI', 20, "bold")).pack(pady=10)
 
         ttk.Label(self, text="Adicione uma Nova API").pack(padx=10, pady=5)
 
